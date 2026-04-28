d309ead25deb9322cde99dbf17230d82eeeee903
Krishna <reachout.anandhu@gmail.com>
2026-04-28 21:42:14 +0530

    Checkpoint

--- code.ts
+++ code.ts
@@ -7,28 +7,60 @@
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__,{ width: 340, height: 480 });
+
+const getTopLevelFrames = () => {
+        const frames = figma.currentPage.children.filter(node => node.type === 'FRAME');
+        return frames.map(f => ({ id: f.id, name: f.name }));
+    };
+
+     setTimeout(() => {
+        figma.ui.postMessage({ type: 'frames-loaded', frames: getTopLevelFrames() });
+    }, 100);
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
+figma.ui.onmessage =  (msg: any) => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-shapes') {
-    // This plugin creates rectangles on the screen.
-    const numberOfRectangles = msg.count;
 
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < numberOfRectangles; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+
+  if (msg.type === 'export-data') {
+     figma.notify("Export triggered"); // 🔥 visible proof
+        figma.getNodeByIdAsync(msg.frameId).then(node=> {
+                   if (!node || node.type !== 'FRAME') {
+                figma.ui.postMessage({
+                    type: "error",
+                    message: "Invalid frame"
+                });
+                return;
+            }
+             const hashNodes = node.findAll(n => n.name.startsWith('#'));
+            console.log("FOUND NODES:", hashNodes.length);
+            if (hashNodes.length === 0) {
+                figma.ui.postMessage({
+                    type: "error",
+                    message: "No # elements found"
+                });
+                return;
+            }
+            const nodeNames = hashNodes.map((n:any) => n.name);
+            // We just need to send the names to UI to generate the CSV headers
+            figma.ui.postMessage({
+                type: 'export-data',
+                data: {
+                    frameName: node.name,
+                    columns: nodeNames
+                }
+            });
+            });
+          
+              
+            
+            // Find all nested nodes starting with '#'
+            // @ts-ignore
+           
   }
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
